9a71b1154e2d26fb6e99576edf27fdbdcfafe716
ammarAlzaqam <git@ammaralzaqam.com>
2025-10-25 01:37:08 +0300

    fix sidebar active link

--- components/global/Sidebar.tsx
+++ components/global/Sidebar.tsx
@@ -18,7 +18,8 @@ export default function Sidebar({
       <div
         className={cn(
           "fixed z-50 top-0 right-0 h-dvh w-dvw max-w-[calc(2*dvw)] bg-primary-900/80 transition-all duration-150",
-          !open && "-translate-x-[100dvw] -translate-y-[100dvh] scale-50 opacity-50"
+          !open &&
+            "-translate-x-[100dvw] -translate-y-[100dvh] scale-50 opacity-50"
         )}
         onClick={() => set(false)}
       />
@@ -31,21 +32,31 @@ export default function Sidebar({
       >
         {/*//! Links */}
         <div className="z-200 pt-14 flex flex-col gap-3">
-          {linksList.map(({ label, route, Icon }) => (
-            <button
-              onClick={() => {
-                router.push(route);
-                set(false);
-              }}
-              key={label}
-              className="cursor-pointer flex items-center p-2 rounded-xl text-secondary-500 hover:bg-secondary-500 hover:text-primary-500! "
-            >
-              <Icon className="size-9 sm:size-12 shrink-0" />
-              <h3 className="flex-1 text-center text-[30px] sm:text-body">
-                {label}
-              </h3>
-            </button>
-          ))}
+          {linksList.map(({ label, route, Icon }) => {
+            const isActive =
+              (pathname.includes(route) && route !== "/") || pathname === route;
+
+            return (
+              <button
+                onClick={() => {
+                  router.push(route);
+                  set(false);
+                }}
+                key={label}
+                className={
+                  cn(
+                    "cursor-pointer flex items-center p-2 rounded-xl text-secondary-500 hover:bg-secondary-500 hover:text-primary-500!",
+                    isActive && "bg-secondary-400 text-primary-500"
+                  )
+                }
+              >
+                <Icon className="size-9 sm:size-12 shrink-0" />
+                <h3 className="flex-1 text-center text-[30px] sm:text-body">
+                  {label}
+                </h3>
+              </button>
+            );
+          })}
         </div>
       </aside>
     </>
